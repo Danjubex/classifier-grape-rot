a9606984550ea7446b5be55d893a377b453b2781
Danillo Xavier <danillox5@hotmail.com>
2018-11-05 13:15:27 -0300

    Regressão Função Não-Linear

--- index.js
+++ index.js
@@ -1,30 +1,82 @@
 
 import * as tf from '@tensorflow/tfjs';
+//
+// // Define a model for linear regression.
+// const model = tf.sequential();
+// model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+//
+// // Prepare the model for training: Specify the loss and the optimizer.
+// model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+//
+// // Train the model using the data.
+//
+//
+//
+// console.log("concluído");
+//
+//
+// // Define input, which has a size of 5 (not including batch dimension).
+// const input = tf.input({shape: [1]});
+//
+// // First dense layer uses relu activation.
+// const denseLayer1 = tf.layers.dense({units: , activation: 'relu'});
+// // Second dense layer uses softmax activation.
+// const denseLayer2 = tf.layers.dense({units: 4, activation: 'softmax'});
+//
+//
+// // Obtain the output symbolic tensor by applying the layers on the input.
+// const output = denseLayer2.apply(denseLayer1.apply(input));
+//
+// // Create the model based on the inputs.
+// const model = tf.model({inputs: input, outputs: output});
+//
+// // The model can be used for training, evaluation and prediction.
+// // For example, the following line runs prediction with the model on
+// // some fake data.
+//
+// // Generate some synthetic data for training.
+// const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+// const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+//
+//
+// model.fit(xs, ys, {epochs: 10}).then(() => {
+//   // Use the model to do inference on a data point the model hasn't seen before:
+//   // console.log("model",model);
+//   model.predict(tf.ones([2, 5])).print();
+//
+// });
 
-// Define a model for linear regression.
+
+//
+// const model1 = tf.sequential();
+// model1.add(tf.layers.dense({inputShape: [4], units: 3, activation: 'relu'}));
+// model1.add(tf.layers.dense({units: 1, activation: 'sigmoid'}));
+// model1.summary();
+// model1.predict(tf.zeros([1, 4])).print();
+
+
 const model = tf.sequential();
 model.add(tf.layers.dense({units: 1, inputShape: [1]}));
 
-// Prepare the model for training: Specify the loss and the optimizer.
-model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+model.add(tf.layers.dense({units: 1, activation: 'relu'}));
 
-// Generate some synthetic data for training.
-const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+// model.add(tf.layers.dense({units: 1, activation: 'sigmoid'}));
 
-// Train the model using the data.
-model.fit(xs, ys, {epochs: 10}).then(() => {
-  // Use the model to do inference on a data point the model hasn't seen before:
-  model.predict(tf.tensor2d([5], [1, 1])).print();
+
+
+model.compile({
+ loss: 'meanSquaredError',
+ optimizer: 'sgd'
 });
 
+const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
+const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
 
-console.log("concluído");
-// import http from 'http';
-//
-// http.createServer((req, res) => {
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   res.end('Hello World\n');
-// }).listen(1337, '127.0.0.1');
-//
-// console.log('Server running at http://127.0.0.1:1337/');
+// const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
+// const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
+
+model.fit(xs, ys, {epochs: 5000}).then(()=>
+{
+  model.predict(tf.tensor2d([4], [1, 1])).print();
+
+});
